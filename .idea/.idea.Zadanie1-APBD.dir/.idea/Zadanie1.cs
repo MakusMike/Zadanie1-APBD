49793b8bc75018c59b3fa0c50e48da0c7eed8cbf
S25713 <s25713@pjwstk.edu.pl>
2024-04-12 23:05:12 +0200

    statyczna funkcja na srednia z tablicy

--- .idea/.idea.Zadanie1-APBD.dir/.idea/Zadanie1.cs
+++ .idea/.idea.Zadanie1-APBD.dir/.idea/Zadanie1.cs
@@ -10,4 +10,19 @@ public class Zadanie1
         
         Console.WriteLine("You are a git noob");
     }
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Array is null or empty.");
+        }
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        return (double)sum / numbers.Length;
+    }
 }
